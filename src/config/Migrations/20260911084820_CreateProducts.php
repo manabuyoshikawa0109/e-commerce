fd86c522a1d034a33e937dedd7e4ac1f5570e2a3
吉川学 <>
2026-09-11 19:33:47 +0900

    商品・商品画像のマイグレーションファイルを追加

---
+++ src/config/Migrations/20260911084820_CreateProducts.php
@@ -0,0 +1,76 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateProducts extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('products', [
+            'collation' => 'utf8mb4_unicode_ci',
+            'comment' => '商品',
+        ]);
+
+        $table
+            ->addColumn('name', 'string', [
+                'limit' => 255,
+                'null' => false,
+                'comment' => '商品名',
+            ])
+            ->addColumn('brand_name', 'string', [
+                'limit' => 255,
+                'null' => true,
+                'comment' => 'ブランド名',
+            ])
+            ->addColumn('category', 'integer', [
+                'limit' => 1,
+                'signed' => false,
+                'null' => false,
+                'comment' => 'カテゴリー',
+            ])
+            ->addColumn('condition', 'integer', [
+                'limit' => 1,
+                'signed' => false,
+                'null' => false,
+                'comment' => '状態',
+            ])
+            ->addColumn('description', 'text', [
+                'null' => true,
+                'default' => null,
+                'comment' => '商品説明',
+            ])
+            // 金額は誤差の出る float ではなく decimal を使う（税抜・円）
+            ->addColumn('price', 'decimal', [
+                'precision' => 10,
+                'scale' => 2,
+                'null' => false,
+                'comment' => '販売価格',
+            ])
+            ->addColumn('is_published', 'boolean', [
+                'default' => false,
+                'null' => false,
+                'comment' => '公開フラグ',
+            ])
+            // CakePHP の Timestamp ビヘイビアが自動で埋めるカラム名
+            ->addColumn('created', 'datetime', [
+                'null' => false,
+                'comment' => '作成日時',
+            ])
+            ->addColumn('modified', 'datetime', [
+                'null' => false,
+                'comment' => '更新日時',
+            ]);
+
+        $table
+            // 一覧で「公開中の商品を新着順」を引くための複合インデックス
+            ->addIndex(['is_published', 'created'], ['name' => 'idx_products_published_created']);
+
+        $table->create();
+    }
+}
---
+++ src/config/Migrations/20260911101148_ProductImages.php
@@ -0,0 +1,48 @@
+<?php
+use Migrations\AbstractMigration;
+
+class ProductImages extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('product_images', [
+            'collation' => 'utf8mb4_unicode_ci',
+            'comment' => '商品画像',
+        ]);
+
+        $table
+            ->addColumn('product_id', 'integer', [
+                'null' => false,
+                'comment' => '商品ID',
+            ])
+            ->addColumn('path', 'string', [
+                'limit' => 255,
+                'null' => false,
+                'comment' => '画像パス',
+            ])
+            ->addColumn('sort_order', 'integer', [
+                'limit' => 1,
+                'signed' => false,
+                'null' => false,
+                'comment' => '並び順',
+            ])
+            // CakePHP の Timestamp ビヘイビアが自動で埋めるカラム名
+            ->addColumn('created', 'datetime', [
+                'null' => false,
+                'comment' => '作成日時',
+            ])
+            ->addColumn('modified', 'datetime', [
+                'null' => false,
+                'comment' => '更新日時',
+            ]);
+
+        $table->create();
+    }
+}
